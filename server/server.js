b49082d4b0e609f7ecec813ffda0e726e0edc224
shaikathaque <shaikathaque4@gmail.com>
2017-08-04 13:42:21 -0700

    Added dummy pun data

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-  res.send('Hello World!');
-});
+var puns = [{question: 'Why can\'t bicycles stand up on their own?', answer: 'because they are two tired.'},
+            {question: 'Who makes the most comfortable shoes?', answer: 'softwear developers'},
+            {question: 'What did the mother tomato say to its child?', answer: 'ketchup!'}];
 
 app.listen(8080, function() {
   console.log('Pun Generator listening on port: 8080');
 });
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/pun', function(req, res) {
+  var pun = puns[getRandomInt(puns.length)]
+  res.send(pun);
+});
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
