cab5c667ee676c04001b71598cd848e3dd5171b2
shaikathaque <shaikathaque4@gmail.com>
2017-08-04 14:50:27 -0700

    added more fake pun data

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 
 var puns = [{question: 'Why can\'t bicycles stand up on their own?', answer: 'because they are two tired.'},
-            {question: 'Who makes the most comfortable shoes?', answer: 'softwear developers'},
-            {question: 'What did the mother tomato say to its child?', answer: 'ketchup!'}];
+            {question: 'Who makes the most comfortable shoes?', answer: 'softwear developers.'},
+            {question: 'What did the mother tomato say to its child?', answer: 'ketchup!'},
+            {question: 'What do you call a cow with no legs?', answer: 'ground beef.'},
+            {question: 'Why did the tomato blush?', answer: 'because it saw the salad dressing.'},
+            {question: 'Why doesn\'t the sun go to college?', answer: 'because it already has 38 million degrees.'},
+            {question: 'Why did the tomato blush?', answer: 'because it saw the salad dressing.'},
+            {question: 'What do you call a beef that twitches?', answer: 'beef jerky.'},
+            {question: 'What do you call a pig that does karate?', answer: 'pork chop.'},
+            {question: 'Why did Santa go to college for music?', answer: 'to improve his wrapping skills.'},
+            {question: 'What did the duck say to the drug dealer?', answer: 'want some quack?'}];
+
 
 app.listen(process.env.PORT || 8080, function() {
   console.log('Pun Generator listening');
