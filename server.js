1489aca7a12d0df9ab07425d6825b853092a4777
shaikathaque <shaikathaque4@gmail.com>
2017-08-05 12:38:03 -0700

    Added CORS header to GET /pun

--- server.js
+++ server.js
@@ -19,6 +19,12 @@ app.all('/', function(req, res, next) {
   next();
  });
 
+ app.all('/pun', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(req, res) {
   res.redirect('/pun');
 });
