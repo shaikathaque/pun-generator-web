6127f97108970d36b76a261d52f1aefa8782c6b0
shaikathaque <shaikathaque4@gmail.com>
2017-08-05 12:34:09 -0700

    Enabled Cross origin request

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.listen(process.env.PORT || 8080, function() {
 
 app.use(bodyParser.json());
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(req, res) {
   res.redirect('/pun');
 });
