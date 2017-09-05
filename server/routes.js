e13e491c76aac2022703467ba335452fcf7d455c
shaikathaque <shaikathaque4@gmail.com>
2017-09-05 06:37:51 -0700

    fix tab size

--- server/routes.js
+++ server/routes.js
@@ -3,36 +3,36 @@ const Pun = require('../db/model/pun');
 
 // helper function
 function getRandomInt(max) {
-	return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 const routes = {
-	getPun: (req, res) => {
-		Pun.find({}).exec()
-			.then( (puns) => {
-				console.log('from routes.js')
-				var randomPun = puns[getRandomInt(puns.length)];
-				res.send(randomPun);
-			})
-			.catch( (err) => {
-				res.sendStatus(404);
-			});	
-	},
+  getPun: (req, res) => {
+    Pun.find({}).exec()
+      .then( (puns) => {
+        console.log('from routes.js')
+        var randomPun = puns[getRandomInt(puns.length)];
+        res.send(randomPun);
+      })
+      .catch( (err) => {
+        res.sendStatus(404);
+      });	
+  },
 
-	addPun: (req, res) => {
-		console.log('addPun from routes.js');
-		let newQuestion = req.body.question;
-		let newAnswer = req.body.answer;
-	
-		let newPun = new Pun({
-			question: newQuestion,
-			answer: newAnswer
-		});
-	
-		newPun.save()
-			.then( (pun) => res.send(pun))
-			.catch(console.error);
-	}
+  addPun: (req, res) => {
+    console.log('addPun from routes.js');
+    let newQuestion = req.body.question;
+    let newAnswer = req.body.answer;
+  
+    let newPun = new Pun({
+      question: newQuestion,
+      answer: newAnswer
+    });
+  
+    newPun.save()
+      .then( (pun) => res.send(pun))
+      .catch(console.error);
+  }
 }
 
 module.exports = routes;
