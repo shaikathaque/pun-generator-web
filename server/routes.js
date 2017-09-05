e21fa82e9afb71a101ebff8ca7596fc76d801326
shaikathaque <shaikathaque4@gmail.com>
2017-09-05 06:13:52 -0700

    add route for POST /pun

--- server/routes.js
+++ server/routes.js
@@ -7,7 +7,7 @@ function getRandomInt(max) {
 }
 
 const routes = {
-	getPuns: (req, res) => {
+	getPun: (req, res) => {
 		Pun.find({}).exec(function(err, puns) {
 			if (err) {
 				res.sendStatus(404);
@@ -15,6 +15,26 @@ const routes = {
 				console.log('from routes.js')
 				var randomPun = puns[getRandomInt(puns.length)];
 				res.send(randomPun);
+			}
+		});
+	},
+	
+	addPun: (req, res) => {
+		console.log('addPun from routes.js');
+		let newQuestion = req.body.question;
+		let newAnswer = req.body.answer;
+	
+		let newPun = new Pun({
+			question: newQuestion,
+			answer: newAnswer
+		});
+	
+		newPun.save(function(err, pun) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(pun);
+				res.send(pun);
 			}
 		});
 	}
