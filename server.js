ee159b1d813f3c9e804edc00a27a25d8dab7b5ed
shaikathaque <shaikathaque4@gmail.com>
2017-09-05 06:14:12 -0700

    refactor addPun logic into routes.js

--- server.js
+++ server.js
@@ -3,20 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const morgan = require('morgan'); 
-
-const db = require('./db/config');
-const Pun = require('./db/model/pun');
 const routes = require('./server/routes');
-
-const app = express();
 const PORT = process.env.PORT || 8000;
 
+const app = express();
+
 app.use(morgan('tiny'));
-
-app.listen(PORT, function() {
-  console.log('Pun Generator listening on port:', PORT);
-});
-
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
@@ -24,30 +16,9 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-app.get('/pun', routes.getPuns);
+app.get('/pun', routes.getPun);
+app.post('/pun', routes.addPun);
 
-app.post('/pun', function(req, res) {
-  let newQuestion = req.body.question;
-  let newAnswer = req.body.answer;
-
-  let newPun = new Pun({
-    question: newQuestion,
-    answer: newAnswer
-  });
-
-  newPun.save(function(err, pun) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(pun);
-      res.send(pun);
-    }
-  });
+app.listen(PORT, function() {
+  console.log('Pun Generator listening on port:', PORT);
 });
-
-//TODO remove
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
-
